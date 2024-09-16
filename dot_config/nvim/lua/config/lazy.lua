8fbb0744db557ffcd777b3485c207ee860e99276
Vasiliy Biryukov <kray74vb@gmail.com>
2024-09-16 19:52:25 +0500

    feat(nvim) disable lazy.nvim checker, enable lazy ui border

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -19,9 +19,10 @@ require("lazy").setup({
 	spec = {
 		{import = "plugins"}
 	},
-	-- Configure any other settings here. See the documentation for more details.
-	-- colorscheme that will be used when installing plugins.
---	install = { colorscheme = { "habamax" } },
-	-- automatically check for plugin updates
-	checker = { enabled = true },
+	ui = {
+		border = "rounded"
+	},
+	checker = {
+		enabled = false
+	}
 })
