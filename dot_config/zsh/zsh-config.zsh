759d708b0a1f529ab91f63fbe558ef4bc79ace56
Vasiliy Biryukov <kray74vb@gmail.com>
2024-10-05 13:35:56 +0500

    feat(zsh) set cursor shape in vi modes

--- dot_config/zsh/zsh-config.zsh
+++ dot_config/zsh/zsh-config.zsh
@@ -11,6 +11,28 @@ setopt AUTO_PUSHD
 setopt PUSHD_IGNORE_DUPS
 setopt PUSHD_SILENT
 
+# Set vi style cursors
+# '\e[1 q' - blinking block
+# '\e[2 q' - non-blinking block
+# '\e[3 q' - blinking underscore
+# '\e[4 q' - non-blinking underscore
+# '\e[5 q' - blinking beam
+# '\e[6 q' - non-blinking beam
+function zle-keymap-select() {
+	if [[ $KEYMAP == vicmd ]]; then
+		echo -ne '\e[1 q'
+	elif [[ $KEYMAP == main ]] || [[ $KEYMAP == viins ]]; then
+		echo -ne '\e[5 q'
+	fi
+}
+
+function zle-line-init() {
+	echo -ne '\e[5 q'
+}
+
+zle -N zle-keymap-select
+zle -N zle-line-init
+
 # Load custom files
 function() {
 	local custom
