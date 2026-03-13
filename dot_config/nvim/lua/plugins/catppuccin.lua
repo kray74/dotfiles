d3f654cd9b6df24a92edb500d43ef444f8b064fc
Vasiliy Biryukov <kray74vb@gmail.com>
2026-03-13 19:29:09 +0500

    fix(nvim) change colorscheme from catppuccin to catppuccin-mocha

--- dot_config/nvim/lua/plugins/catppuccin.lua
+++
@@ -1,6 +0,0 @@
-return {
-  "catppuccin",
-  opts = {
-    transparent_background = true,
-  },
-}
--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,16 @@
 return {
   {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      transparent_background = true,
+    },
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      colorscheme = "catppuccin-mocha",
     },
   },
 }
