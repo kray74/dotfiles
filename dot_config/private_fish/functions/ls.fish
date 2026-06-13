f51dafc702a0bfb37baca892d764cb5e9d1c4201
Vasiliy Biryukov <kray74vb@gmail.com>
2026-06-13 09:14:57 +0500

    feat(fish): replace -a to -A in ls function

--- dot_config/private_fish/functions/ls.fish
+++ dot_config/private_fish/functions/ls.fish
@@ -1,4 +1,4 @@
 function ls --description 'alias ls -lahF --color=auto --hyperlink=auto'
-    command ls -lahF --color=auto --hyperlink=auto $argv
+    command ls -lAhF --color=auto --hyperlink=auto $argv
 
 end
